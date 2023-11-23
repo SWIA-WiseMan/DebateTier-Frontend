9eb05836379103afe197780cadb36796b077afbe
Ga Dong Sik <ret0422@gmail.com>
2023-11-23 20:16:58 +0900

    feat: Navigation 적용
    폴더 구조 정리
    Navigation 적용
    관련 npm 설치

--- app/App.tsx
+++ app/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "@screens/main/HomeScreen";
+import IssueScreen from "@screens/main/IssueScreen";
+import MypageScreen from "@screens/main/MypageScreen";
 
-import Header from "./components/common/Header.tsx";
-import IssueList from "./components/IssueList.tsx";
+export type RootStackParamList = {
+	Home: undefined;
+	Issue: undefined;
+	Mypage: undefined;
+};
 
-function App(): JSX.Element {
-	const handleSharePress = () => {};
+const Stack = createStackNavigator<RootStackParamList>();
 
+const App: React.FC = () => {
 	return (
-		<SafeAreaView>
-			<StatusBar />
-			<Header showShareButton={true} onSharePress={handleSharePress} />
-			<ScrollView contentInsetAdjustmentBehavior="automatic">
-				<IssueList />
-			</ScrollView>
-		</SafeAreaView>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Issue" component={IssueScreen} />
+				<Stack.Screen name="Mypage" component={MypageScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
-}
+};
 
 export default App;
