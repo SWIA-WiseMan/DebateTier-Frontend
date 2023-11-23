0c4d465fd8cf9d73dd2c547dc81b81685e052b58
Ga Dong Sik <ret0422@gmail.com>
2023-11-23 22:21:54 +0900

    refactor: stack 네비를 tab 네비로 변경
    하면서 stack npm uninstall

--- app/App.tsx
+++ app/App.tsx
@@ -1,26 +1,50 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import HomeScreen from "@screens/main/HomeScreen";
 import IssueScreen from "@screens/main/IssueScreen";
 import MypageScreen from "@screens/main/MypageScreen";
 
-export type RootStackParamList = {
-	Home: undefined;
-	Issue: undefined;
-	Mypage: undefined;
-};
+import HomeIcon from "@assets/images/Icon_Home.svg";
+import IssueIcon from "@assets/images/Icon_Issue.svg";
+import MypageIcon from "@assets/images/Icon_Mypage.svg";
 
-const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Issue" component={IssueScreen} />
-				<Stack.Screen name="Mypage" component={MypageScreen} />
-			</Stack.Navigator>
+			<Tab.Navigator
+				initialRouteName="Home"
+				screenOptions={{
+					tabBarActiveTintColor: "#FFB800",
+					tabBarInactiveTintColor: "#BDBDBD",
+					headerShown: false,
+				}}
+			>
+				<Tab.Screen
+					name="Home"
+					component={HomeScreen}
+					options={() => ({
+						tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+					})}
+				/>
+				<Tab.Screen
+					name="Issue"
+					component={IssueScreen}
+					options={() => ({
+						tabBarIcon: ({ color }) => <IssueIcon color={color} />,
+					})}
+				/>
+				<Tab.Screen
+					name="Mypage"
+					component={MypageScreen}
+					options={() => ({
+						tabBarIcon: ({ color }) => <MypageIcon color={color} />,
+					})}
+				/>
+			</Tab.Navigator>
 		</NavigationContainer>
 	);
 };
