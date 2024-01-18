1febd353b175dbd3ae615bf2847c93002e25d2fa
daeng <jyjo522i@gmail.com>
2024-01-18 20:58:30 +0900

    fix: 메시지창 수정

--- app/components/debate/ChatRoom.tsx
+++ app/components/debate/ChatRoom.tsx
@@ -1,75 +1,98 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 
-import Icon_Mask from "@assets/images/Icon_Mask.svg";
-import Icon_Search from "@assets/images/Icon_Search.svg";
-import Icon_Alert from "@assets/images/Icon_Alert.svg";
+import Icon_Profile from "@assets/images/Icon_Profile.svg";
+import Icon_Add from "@assets/images/debate/Icon_Add.svg";
+import Icon_Send from "@assets/images/debate/Icon_Send.svg";
+import Icon_ChatArrow from "@assets/images/debate/Icon_ChatArrow.svg"
+import { TextInput } from "react-native-gesture-handler";
 
-interface ProfileData {
-	name: string;
-}
 
-const HomeProfile: React.FC<ProfileData> = ({ name }) => (
-	<View style={styles.container}>
-		<View style={styles.rowContainer}>
-			<Icon_Mask />
-			<Text style={styles.highLightText}>{name}</Text>
-			<Text style={styles.simpleText}>님 환영해요!</Text>
-		</View>
-		<View style={styles.rowContainer}>
-			<Icon_Search style={{ marginRight: 12 }} />
-			<Icon_Alert />
-		</View>
-	</View>
-);
+const ChatRoom: React.FC = () => {
+	const [text, setText] = useState('');
+	return (
+		<>
+		 <KeyboardAvoidingView
+            style={styles.container}
+        >
+			<View style={styles.container}>
+				<View style={styles.rowContainer}>
+					<Icon_Profile style={styles.iconMargin}/>
+					<Text style={styles.simpleText}>잠자는 숲속의 나무늘보</Text>
+				</View>
+				<View style={[styles.rowContainer,styles.chatMargin]}>
+					<Icon_ChatArrow/>
+					<View style={styles.chatContainer}>
+					<Text style={styles.simpleText}>이건 진짜 아니지예... 토맛? 윽.. 그냥 토마토맛 토가 나음;;</Text>
+					</View>
+				</View>
+
+				<View style={[styles.mesaageContainer, {marginTop: 580}]}>
+					<Icon_Add style={{marginLeft: 5}}/>
+					<TextInput
+					  style={styles.textInput} // 크기 조정을 위한 스타일 추가
+					  onChangeText={setText} // 텍스트 변경을 처리하는 핸들러
+					  value={text} // 현재 텍스트 값
+					  placeholder="OO님의 의견을 말해주세요."
+					  placeholderTextColor="lightgray"
+					/>
+					<Icon_Send style={styles.iconMargin}/>
+				</View>
+			</View>
+			</KeyboardAvoidingView>
+	</>);
+};
 
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		marginTop: 16,
-		marginLeft: 16,
-		marginRight: 16,
+		flex: 1,
+		width: "100%",
+		backgroundColor: "#FFFFFF",
 	},
 	rowContainer: {
 		flexDirection: "row",
-		alignItems: "center",
+		alignItems: "center"
 	},
-	commonText: {
-		textAlign: "center",
-		fontFamily: "NotoSansKR-Bold",
-		fontSize: 16,
-		fontWeight: "500",
-		lineHeight: 20,
-		letterSpacing: -1,
+	iconMargin: {
+		marginRight:5,
 	},
-	highLightText: {
-		color: "#FFB800",
-		textAlign: "center",
-		fontFamily: "NotoSansKR-Bold",
-		fontSize: 16,
-		fontWeight: "500",
-		lineHeight: 20,
-		letterSpacing: -1,
+	chatMargin: {
+		marginLeft: 10,
+	},
+	chatContainer: {
+		backgroundColor: "#D9D9D9",
+		width: 350,
+		height: 32,
+		marginTop: 10,
+		borderRadius: 6
 	},
 	simpleText: {
 		color: "#000000",
 		textAlign: "center",
 		fontFamily: "NotoSansKR-Bold",
-		fontSize: 16,
+		fontSize: 14,
 		fontWeight: "500",
 		lineHeight: 20,
 		letterSpacing: -1,
 	},
-	// commonText: {
-	// 	textAlign: "center",
-	// 	fontFamily: "NotoSansKR-Bold",
-	// 	fontSize: 16,
-	// 	fontWeight: "500",
-	// 	lineHeight: 20,
-	// 	letterSpacing: -1,
-	// },
+	mesaageContainer: {
+		position: 'absolute',
+		bottom: 0,
+		flexDirection: "row",
+		justifyContent:"space-between",
+		alignItems: "center",
+		backgroundColor: "#F7F7F7",
+		width: "92%",
+		height: 40,
+		marginLeft: 16,
+		marginRight: 16,
+		marginBottom: 16,
+		borderRadius: 10,
+	},
+	textInput: {
+        width: 300,
+        height: 40,
+    },
 });
 
-export default HomeProfile;
+export default ChatRoom;
--- app/screens/main/DebateScreen.tsx
+++ app/screens/main/DebateScreen.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView, View, Text } from "react-native";
 
 import Header from "@components/common/Header";
 import DebateList from "@components/debate/DebateList";
+import ChatRoom from "@components/debate/ChatRoom";
 
 const DebateScreen: React.FC = () => {
 	const handleSharePress = () => {};
@@ -11,7 +12,8 @@ const DebateScreen: React.FC = () => {
 		<SafeAreaView style={{ flex: 1 }}>
 			<Header showShareButton={false} onSharePress={handleSharePress} />
 			<View style={{ flex: 1 }}>
-				<DebateList layout="vertical" />
+				{/* <DebateList layout="vertical" /> */}
+				<ChatRoom/>
 			</View>
 		</SafeAreaView>
 	);
